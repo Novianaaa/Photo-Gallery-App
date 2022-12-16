9782fb1ba5cba2b60913f5015f12b5829ab84215
Novianaaa <novianasr@students.amikom.ac.id>
2022-12-16 22:44:40 +0700

    edit task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://photo-galery-app-fe3163876.netlify.app/" // TODO: replace this
+const studentName = "Noviana Sami Ratri" // TODO: replace this
+const studentId = "FE3163876" // TODO: replace this
 
 export { url, studentName, studentId }
